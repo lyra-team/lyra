8714cfdd575ff1c63e2692b2fd32ec53b96b2118
Polarniy Nickolay <PolarNick239@gmail.com>
2015-03-21 20:44:26 +0300

    Action!

--- src/ts/game.ts
+++ src/ts/game.ts
@@ -18,12 +18,16 @@ module game {
         private colBuf:webgl.ArrayBuffer;
         private indBuf:webgl.ElementArrayBuffer;
 
+        private startTime;
+
         constructor(rootId) {
             this.root = ui.$(rootId);
             this.canvas = ui.$$<HTMLCanvasElement>("." + C_GAME_CANVAS, this.root);
 
             this.initWebGL();
             this.initInputEvents();
+
+            this.startTime = new Date().getTime();
         }
 
         private initWebGL() {
@@ -55,79 +59,64 @@ module game {
             this.canvas.height = window.innerHeight;
         }
 
-        private createPoints() {
-            return new Float32Array([
-                4, 0, 0,
-                4, 0, 1,
-                4, 1, 0,
-                4, 1, 1,
-                5, 0, 0,
-                5, 0, 1,
-                5, 1, 0,
-                5, 1, 1,
-                4, 0, 0,
-                4, 0, 1,
-                4, 1, 0,
-                4, 1, 1,
-                5, 0, 0,
-                5, 0, 1,
-                5, 1, 0,
-                5, 1, 1,
-                4, 0, 0,
-                4, 0, 1,
-                4, 1, 0,
-                4, 1, 1,
-                5, 0, 0,
-                5, 0, 1,
-                5, 1, 0,
-                5, 1, 1
-            ]);
+        private createPoints(points, n, splinesN) {
+            var res = new Float32Array((n - 1) * splinesN * 2 * 3 * 3);
+            var next = 0;
+            for (var i = 0; i < n - 1; i++) {
+                for (var j = 0; j < splinesN; j++) {
+                    var ax = points[i * (splinesN + 1) * 3 + j * 3 + 0], ay = points[i * (splinesN + 1) * 3 + j * 3 + 1], az = points[i * (splinesN + 1) * 3 + j * 3 + 2];
+                    var bx = points[i * (splinesN + 1) * 3 + (j + 1) * 3 + 0], by = points[i * (splinesN + 1) * 3 + (j + 1) * 3 + 1], bz = points[i * (splinesN + 1) * 3 + (j + 1) * 3 + 2];
+                    var cx = points[(i + 1) * (splinesN + 1) * 3 + j * 3 + 0], cy = points[(i + 1) * (splinesN + 1) * 3 + j * 3 + 1], cz = points[(i + 1) * (splinesN + 1) * 3 + j * 3 + 2];
+                    var dx = points[(i + 1) * (splinesN + 1) * 3 + (j + 1) * 3 + 0], dy = points[(i + 1) * (splinesN + 1) * 3 + (j + 1) * 3 + 1], dz = points[(i + 1) * (splinesN + 1) * 3 + (j + 1) * 3 + 2];
+                    res[next * 3] = ax, res[next * 3 + 1] = ay, res[next * 3 + 2] = az;
+                    next += 1;
+                    res[next * 3] = bx, res[next * 3 + 1] = by, res[next * 3 + 2] = bz;
+                    next += 1;
+                    res[next * 3] = cx, res[next * 3 + 1] = cy, res[next * 3 + 2] = cz;
+                    next += 1;
+
+                    res[next * 3] = bx, res[next * 3 + 1] = by, res[next * 3 + 2] = bz;
+                    next += 1;
+                    res[next * 3] = cx, res[next * 3 + 1] = cy, res[next * 3 + 2] = cz;
+                    next += 1;
+                    res[next * 3] = dx, res[next * 3 + 1] = dy, res[next * 3 + 2] = dz;
+                    next += 1;
+                }
+            }
+            return res;
         }
 
-        private createColors() {
-            return new Float32Array([
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 0
-            ]);
+        private createColors(n, r, g, b) {
+            var colors = new Float32Array(n * 3);
+            for (var i = 0; i < n; i++) {
+                colors[i * 3] = r;
+                colors[i * 3 + 1] = g;
+                colors[i * 3 + 2] = b;
+            }
+            return colors;
         }
 
-        private createIndicies() {
-            return new Uint16Array([
-                0, 1, 2,
-                1, 2, 3,
-                4, 5, 6,
-                5, 6, 7,
-                8+1, 8+3, 8+5,
-                8+3, 8+5, 8+7,
-                8+0, 8+2, 8+4,
-                8+2, 8+4, 8+6,
-                16+2, 16+3, 16+6,
-                16+3, 16+6, 16+7,
-                16+0, 16+1, 16+4,
-                16+1, 16+4, 16+5
-            ]);
+        private createIndicies(n, splinesN) {
+            var indicies = new Uint16Array((n - 1) * splinesN * 2 * 3);
+            console.assert(indicies.length <= 65535);
+            var next = 0;
+            for (var i = 0; i < n - 1; i++) {
+                for (var j = 0; j < splinesN; j++) {
+                    indicies[next] = next;
+                    next += 1;
+                    indicies[next] = next;
+                    next += 1;
+                    indicies[next] = next;
+                    next += 1;
+                    indicies[next] = next;
+                    next += 1;
+                    indicies[next] = next;
+                    next += 1;
+                    indicies[next] = next;
+                    next += 1;
+                }
+            }
+            return indicies;
         }
 
         private generateNormals(points, indicies) {
@@ -150,14 +139,14 @@ module game {
             return normals;
         }
 
-        private createCameraMtx(x, y, z, angleOfView, course, pitch) {
+        private createCameraMtx(x, y, z, angleOfView) {
             var pMatrix = mat4.create();
             mat4.perspective(angleOfView, this.canvas.width / this.canvas.height, 0.1, 100.0, pMatrix);
             var mvMatrix = mat4.create();
             mat4.identity(mvMatrix);
             mat4.translate(mvMatrix, [-x, -y, -z]);
-            mat4.rotate(mvMatrix, Math.PI / 2 - pitch, [0, 1, 0]);
-            mat4.rotate(mvMatrix, -course, [0, 0, 1]);
+            mat4.rotate(mvMatrix, Math.PI / 2, [0, 1, 0]);
+            mat4.rotate(mvMatrix, -Math.PI / 2, [1, 0, 0]);
             var cameraMtx = mat4.create();
             mat4.multiply(pMatrix, mvMatrix, cameraMtx);
             return cameraMtx;
@@ -177,9 +166,19 @@ module game {
         private loop() {
             this.makeFullPage();
 
-            var points = this.createPoints();
-            var colors = this.createColors();
-            var indicies = this.createIndicies();
+            var points = this.createPoints(new Float32Array([
+                4, 2, 1,
+                4, 1, 2,
+                4, 0, 2.2,
+                4, -1, 2,
+                4, -2, 1,
+                6, 2, 1,
+                6, 1, 2,
+                6, 0, 2.2,
+                6, -1, 2,
+                6, -2, 1]), 2, 4);
+            var colors = this.createColors(4*2*3, 0, 1, 0);
+            var indicies = this.createIndicies(2, 4);
             var normals = this.generateNormals(points, indicies);
 
             this.posBuf.uploadData(points);
@@ -191,15 +190,16 @@ module game {
             this.mapShader.vertexAttribute('aPosition', this.posBuf);
             this.mapShader.vertexAttribute('aColor', this.colBuf);
 
-            var time = new Date().getTime();
-            var camX = 2.0*Math.sin(time / 1000.0), camY = 0, camZ = 3;
-            var course = Math.PI / 6, pitch = 0, viewAngleVert = 45;
-            this.mapShader.uniformMatrixF('uCameraMtx', this.createCameraMtx(camX, camY, camZ, viewAngleVert, course, pitch));
+            var curTime = new Date().getTime();
+            var time = (curTime - this.startTime) / 1000.0;
+            var camX = 0, camY = 3, camZ = 0 - time;
+            var viewAngleVert = 45;
+            this.mapShader.uniformMatrixF('uCameraMtx', this.createCameraMtx(camX, camY, camZ, viewAngleVert));
             this.mapShader.uniformF('uCameraPosition', camX, camY, camZ);
-            this.setUniformLight('uLight', camX, camY, camZ, 2.0, 0.1, 0.0);
+            this.setUniformLight('uLight', camX, camY, camZ, 2.0, 0.1, 0.5);
 
-            gl.clear(gl.DEPTH | gl.COLOR);
             gl.enable(gl.DEPTH_TEST);
+            gl.clear(gl.DEPTH | gl.COLOR);
             this.mapShader.draw(this.canvas.width, this.canvas.height, gl.TRIANGLES, this.indBuf);
             window.requestAnimationFrame(this.loop.bind(this));
         }
