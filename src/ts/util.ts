b17c0f5cfaab6384a2da89aee4af5547b0863a7b
zyulyaev <nikita@zyulyaev.ru>
2015-03-22 14:53:28 +0300

    block generation

--- src/ts/util.ts
+++ src/ts/util.ts
@@ -4,4 +4,12 @@ module util {
     export function pickVec3(coords, idx): vec3 {
         return [].slice.call(coords, idx * 3, idx * 3 + 3);
     }
+
+    export function putVec3(coords: number[], idx:number, vec: vec3);
+    export function putVec3(coords: ArrayBufferView, idx:number, vec: vec3);
+    export function putVec3(coords, idx, vec) {
+        coords[idx * 3] = vec[0];
+        coords[idx * 3 + 1] = vec[1];
+        coords[idx * 3 + 2] = vec[2];
+    }
 }
--- src/ts/webgl/map.ts
+++ src/ts/webgl/map.ts
@@ -33,4 +33,42 @@ module map {
         return points;
     }
 
+    function av(a: vec3, b: vec3): vec3 {
+        return vec3.scale(vec3.add(a, b, []), 0.5);
+    }
+
+    export function generateBlocks(sectorsPoints: Float32Array, blocks, stripCount: number, blockSize: vec3) {
+        var result = new Float32Array(blocks.length * 8 * 3);
+        blocks.forEach((block, idx) => {
+            var kp = block[0],
+                strip = block[1];
+
+            var stripOffset = kp * (stripCount + 1) + strip,
+                bottomLeft = util.pickVec3(sectorsPoints, stripOffset),
+                bottomRight = util.pickVec3(sectorsPoints, stripOffset + 1),
+                topLeft = util.pickVec3(sectorsPoints, stripOffset + stripCount + 1),
+                topRight = util.pickVec3(sectorsPoints, stripOffset + stripCount + 2);
+
+            var stripCenter = av(topRight, bottomLeft),
+                dx = vec3.scale(vec3.direction(av(topLeft, topRight), av(bottomLeft, bottomRight)), blockSize[0] / 2),
+                dy = vec3.scale(vec3.direction(av(topRight, bottomRight), av(topLeft, bottomLeft)), blockSize[1] / 2),
+                dz = vec3.scale(vec3.normalize(vec3.cross(dx, dy)), blockSize[2]),
+
+                dxdy = vec3.add(dx, dy, []),
+                dxmdy = vec3.subtract(dx, dy, []),
+                dxdydz = vec3.add(dxdy, dz, []),
+                dxmdydz = vec3.add(dxmdy, dz, []);
+
+            util.putVec3(result, idx * 8, vec3.add(stripCenter, dxdy, []));
+            util.putVec3(result, idx * 8 + 1, vec3.add(stripCenter, dxmdy, []));
+            util.putVec3(result, idx * 8 + 2, vec3.subtract(stripCenter, dxdy, []));
+            util.putVec3(result, idx * 8 + 3, vec3.subtract(stripCenter, dxmdy, []));
+
+            util.putVec3(result, idx * 8 + 4, vec3.add(stripCenter, dxdydz, []));
+            util.putVec3(result, idx * 8 + 5, vec3.add(stripCenter, dxmdydz, []));
+            util.putVec3(result, idx * 8 + 6, vec3.subtract(stripCenter, dxdydz, []));
+            util.putVec3(result, idx * 8 + 7, vec3.subtract(stripCenter, dxmdydz, []));
+        });
+        return result;
+    }
 }
